696891f718ecf538fd27a5690aced76fa5522e53
sush-tech <sushmithar31@gmail.com>
2023-09-25 21:15:36 -0400

    adding most of the work

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,53 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+setInterval(scheduleTracker, 1000); 
+function scheduleTracker() {
+  let today = dayjs();
+  $("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm:ss a")); //current date time format
+
+  var currentHour = today.hour(); 
+  // for the colour variations for timeblock
+  $(".time-block").each(function () {
+    var savedHour = parseInt($(this).attr("id").split("hour-")[1]);
+    console.log( savedHour, currentHour)
+
+    //changes the past present future according to the current time
+    if (savedHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future present");
+    }
+    else if (savedHour === currentHour) {
+        $(this).addClass("present");
+        $(this).removeClass("future past");
+    }
+    else {
+        $(this).addClass("future");
+        $(this).removeClass("past present");
+    }
+})
+scheduleTracker();
+}
+$('.saveBtn').click(function(){
+  let textArea = $(this).siblings(".description").val();
+  let currentDay =  $(this).parent().attr("id");
+
+  localStorage.setItem(currentDay, textArea);
+})
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+
+// instead of dayjs() we can use moment(), more fast and accurate results
+
+
+
+
+
+
+
+
